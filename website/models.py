cf04a237f896d2527a6c717592dde237269ffb84
muratseyfi <muratseyfitr@gmail.com>
2024-02-16 11:31:23 +0300

    Authentication and CRUD

--- website/models.py
+++ website/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    meet_subject = models.CharField(max_length=200)
+    organizer = models.CharField(max_length=100)
+    participants = models.TextField()
+    meet_date = models.DateField(auto_now=True)
+    start_time = models.TimeField(auto_now=True)
+    end_time = models.TimeField(auto_now=True)
+   
+
+    def __str__(self):
+        return(f"{self.meet_subject} {self.organizer}")
